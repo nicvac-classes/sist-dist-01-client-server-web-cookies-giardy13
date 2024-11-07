6a50bd37be7822e28c2679a4c2f5d7bc3cbd77ce
giardy13 <mattiadomenico.giardino@marconibari.edu.it>
2024-11-07 18:34:22 +0000

    esercizio

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
+const cookieParser = require('cookie-parser'); 
 
 // Configurazione EJS
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cookieParser());
 app.get('/', (req, res) => {
-    res.render('form');
+    const name = req.cookies.name; // Legge il cookie "name"
+    if (name) {
+        // Se il cookie esiste, mostra la pagina di saluto
+        res.render('greet', { message:'Bentornato', name: name });
+    } else {
+        // Se non esiste, mostra il form
+        res.render('form');
+    }
 });
 
-app.post('/greet', (req, res) => {
-    const name = req.body.name;
-    res.render('greet', { name: name });
+app.post('/logout', (req, res) => {
+    res.clearCookie('name'); // Cancella il cookie chiamato "name"
+    res.redirect('/'); // Reindirizza alla home page
 });
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
